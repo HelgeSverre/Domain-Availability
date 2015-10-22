1889ad8cb4e6e970f18377cfabbfa47fdc9132b2
Helge Sverre <helge.sverre@gmail.com>
2015-10-22 15:37:44 +0200

    Made the example more sexy

--- example.php
+++ example.php
@@ -12,26 +12,50 @@ use Helge\Loader\JsonLoader;
 use Helge\Client\SimpleWhoisClient;
 use Helge\Service\DomainAvailability;
 
-$whoisClient    = new SimpleWhoisClient();
-$dataLoader     = new JsonLoader("src/data/servers.json");
+$whoisClient = new SimpleWhoisClient();
+$dataLoader = new JsonLoader("src/data/servers.json");
 
-$service        = new DomainAvailability($whoisClient, $dataLoader);
-
-if (isset($_GET["domain"])) {
-    if ($service->isAvailable($_GET["domain"])) {
-        echo "<h2 style='color:green;'>Available</h2>";
-    } else {
-        echo "<h2 style='color:red;'>Unavailable</h2>";
-    }
-}
-
+$service = new DomainAvailability($whoisClient, $dataLoader);
 ?>
 
-<form action="" method="get">
-    <label for="domain"></label>
-    <input type="text" name="domain" id="domain">
-    <input type="submit" value="Go">
-</form>
+<div>
+    <form style="margin: 10px;" action="" method="get">
+        <label for="domain">Domain Name</label>
+        <input type="text" name="domain" id="domain">
+        <input type="submit" value="Go">
+    </form>
+
+    <table border="1" cellpadding="5">
+        <tr>
+            <td>Status</td>
+            <td>
+                <?php
+                if (isset($_GET["domain"])) {
+                    if ($service->isAvailable($_GET["domain"])) {
+                        echo "<span style='color:green;'>Available</span>";
+                    } else {
+                        echo "<span style='color:red;'>Unavailable</span>";
+                    }
+                }
+                ?>
+            </td>
+        </tr>
+        <tr>
+            <td>WHOIS Server</td>
+            <td><?= $whoisClient->getServer() ?></td>
+        </tr>
+        <tr>
+            <td>Server Port</td>
+            <td><?= $whoisClient->getPort() ?></td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <pre><?= $whoisClient->getResponse(); ?></pre>
+            </td>
+        </tr>
+    </table>
+
+</div>
 </body>
 </html>
 
